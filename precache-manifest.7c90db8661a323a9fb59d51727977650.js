adf6fba7c25d21756d9909bf9ba571bf99b570f8
anantpatni1 <anantpatni1@gmail.com>
2018-12-18 16:56:09 +0530

    Updates

---
+++ precache-manifest.7c90db8661a323a9fb59d51727977650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4404720ece11355df318a0acf525cb1",
+    "url": "/tv-series/static/media/loader.f4404720.gif"
+  },
+  {
+    "revision": "01bf3f0effbc4517ceed",
+    "url": "/tv-series/static/js/runtime~main.01bf3f0e.js"
+  },
+  {
+    "revision": "d651dbf3bb0ab30c06ea",
+    "url": "/tv-series/static/js/main.d651dbf3.chunk.js"
+  },
+  {
+    "revision": "46475e92f7c3484a377f",
+    "url": "/tv-series/static/js/1.46475e92.chunk.js"
+  },
+  {
+    "revision": "d651dbf3bb0ab30c06ea",
+    "url": "/tv-series/static/css/main.0fe620c4.chunk.css"
+  },
+  {
+    "revision": "0ad8ac23ec27f7d1806dfdab1e1dce44",
+    "url": "/tv-series/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tv-series-app/precache-manifest.e248ac298239f721c207bc42ce227ee8.js"
+  "/tv-series/precache-manifest.7c90db8661a323a9fb59d51727977650.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tv-series-app/index.html", {
+workbox.routing.registerNavigationRoute("/tv-series/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
